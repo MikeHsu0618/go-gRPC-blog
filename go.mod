9b0aa79e3757f290a0022763ee97f37bf72b55be
Mike <a3698521tw11@gmail.com>
2022-04-30 15:32:27 +0800

    docs: "add grpc gateway"

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module go-grpc-blog
 go 1.18
 
 require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
+	go.mongodb.org/mongo-driver v1.9.0
+	google.golang.org/genproto v0.0.0-20220317150908-0efb43f6373e
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
 )
@@ -17,11 +20,9 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.5 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	pb "go-grpc-blog/proto"
 	"go-grpc-blog/server/repository"
 	"go-grpc-blog/server/service"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var collection *mongo.Collection
 var addr = "0.0.0.0:50051"
+var restful = "8080"
 
 func main() {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@localhost:27017/"))
@@ -40,10 +45,37 @@ func main() {
 	repo := repository.NewRepository(collection)
 	svc := service.NewService(repo)
 
+	go httpServer()
 	s := grpc.NewServer()
 	pb.RegisterBlogServiceServer(s, svc)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v\n", err)
 	}
+
+}
+
+func httpServer() {
+	conn, err := grpc.Dial(
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = pb.RegisterBlogServiceHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    fmt.Sprintf(":%s", restful),
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0" + fmt.Sprintf(":%s", restful))
+	log.Fatalln(gwServer.ListenAndServe())
 }
